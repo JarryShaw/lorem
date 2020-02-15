fcd4c3078cb520fd8f04188a98034a9e5a99990e
Jarry Shaw <jarryshaw@icloud.com>
2020-02-15 11:39:21 +0800

    No changes

--- setup.py
+++ setup.py
@@ -1,90 +1,87 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=docstring-first-line-empty
-"""
-Lorem Ipsum Generator
-=====================
+"""Lorem ipsum generator.
 
 In publishing and graphic design, lorem ipsum is a placeholder text commonly
 used to demonstrate the visual form of a document or a typeface without
 relying on meaningful content.
 
-The ``lorem`` module provides a generic access to generating the lorem ipsum
-text from its very original text::
+The `lorem` module provides a generic access to generating the lorem ipsum text
+from its very original text:
 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-    est laborum.
+> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+> tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+> veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+> commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+> esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+> cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+> est laborum.
 
-Usage of the ``lorem`` module is rather simple. Depending on your needs, the
-``lorem`` module provides generation of *words*, *sentences*, and
-*paragraphs*.
+Usage of the `lorem` module is rather simple. Depending on your needs, the
+`lorem` module provides generation of **word**s, **sentence**s, and
+**paragraph**s.
 
 Get Random Words
 ----------------
 
-The ``lorem`` module provides two different ways for getting random words.
+The `lorem` module provides two different ways for getting random words.
 
-1. :func:`word` -- generate a list of random words
+1. `word` -- generate a list of random words
 
-   .. code:: python
+   ```python
+   word(count=1, func=None, args=[], kwargs={}) -> Iterable[str]
+   ```
 
-      word(count=1, func=None, args=(), kwargs={}) -> Iterator[str]
+2. `get_word` -- return random words
 
-2. :func:`get_word` -- return random words
-
-   .. code:: python
-
-      get_word(count=1, sep=' ', func=None, args=(), kwargs={}) -> str
+   ```python
+   get_word(count=1, sep=' ', func=None, args=[], kwargs={}) -> str
+   ```
 
 Get Random Sentences
 --------------------
 
-The ``lorem`` module provides two different ways for getting random sentences.
+The `lorem` module provides two different ways for getting random sentences.
 
-1. :func:`sentence` -- generate a list of random sentences
+1. `sentence` -- generate a list of random sentences
 
-   .. code:: python
+   ```python
+   sentence(count=1, comma=(0, 2), word_range=(4, 8)) -> Iterable[str]
+   ```
 
-      sentence(count=1, comma=(0, 2), word_range=(4, 8)) -> Iterator[str]
+2. `get_sentence` -- return random sentences
 
-2. :func:`get_sentence` -- return random sentences
-
-   .. code :: python
-
-      get_sentence(count=1, sep=' ', comma=(0, 2), word_range=(4, 8)) -> Union[str]
+   ```python
+   get_sentence(count=1, comma=(0, 2), word_range=(4, 8), sep=' ') -> Union[str]
+   ```
 
 Get Random Paragraphs
 ---------------------
 
-The ``lorem`` module provides two different ways for getting random paragraphs.
+The `lorem` module provides two different ways for getting random paragraphs.
 
-1. :func:`paragraph` -- generate a list of random paragraphs
+1. `paragraph` -- generate a list of random paragraphs
 
-   .. code:: python
+   ```python
+   paragraph(count=1, comma=(0, 2), word_range=(4, 8), sentence_range=(5, 10)) -> Iterable[str]
+   ```
 
-      paragraph(count=1, comma=(0, 2), word_range=(4, 8), sentence_range=(5, 10)) -> Iterator[str]
+2. `get_paragraph` -- return random paragraphs
 
-2. :func:`get_paragraph` -- return random paragraphs
-
-   .. code:: python
-
-      get_paragraph(count=1, sep=os.linesep, comma=(0, 2), word_range=(4, 8), sentence_range=(5, 10)) -> Union[str]
+   ```python
+   get_paragraph(count=1, comma=(0, 2), word_range=(4, 8), sentence_range=(5, 10), sep=os.linesep) -> Union[str]
+   ```
 
 Customise Word Pool
 -------------------
 
-If wanted, the ``lorem`` module also provides an interface to customise the word
+If wanted, the `lorem` module also provides an interface to customise the word
 pool as you wish.
 
-1. :func:`set_pool` -- customise random word pool
+1. `set_pool` -- customise random word pool
 
-   .. code:: python
-
-      set_pool(pool)
+   ```python
+   set_pool(pool)
+   ```
 
 """
 # version string
@@ -174,7 +171,7 @@ try:
         # include_dirs
         # password
         # fullname
-        long_description_content_type='text/x-rst',
+        long_description_content_type='text/markdown',
         python_requires='>=3.5',
         zip_safe=True,
     ))
