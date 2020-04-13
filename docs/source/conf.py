64db16215c14aba2e33711ffc9c823d84e9e34ab
Jarry Shaw <jarryshaw@icloud.com>
2020-04-13 19:36:01 +0800

    Introduct sphinx.ext.viewcode

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ release = '1.1.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
     'sphinx.ext.autodoc', 'sphinx.ext.autodoc.typehints',
     'sphinxcontrib.napoleon'
