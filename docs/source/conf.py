858d83da7a0367d853d5b1b0abfbbaf8590a73bd
Jarry Shaw <jarryshaw@icloud.com>
2020-10-08 01:16:57 +0800

    updated Pipfile

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,6 +88,7 @@ html_theme_options = {
     'github_user': 'JarryShaw',
     'github_repo': 'lorem',
     'github_banner': True,
+    'github_type': 'star',
     #'show_related': False,
     #'note_bg': '#FFF59C',
     #'travis_button': True,
