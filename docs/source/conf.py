b3f9cc66b972a3c44fc8fedc832489284dc960e6
Jarry Shaw <jarryshaw@icloud.com>
2023-04-23 13:46:01 +0800

    bugfix for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,19 +4,18 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import datetime
-
-import lorem
-
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+import datetime
+import lorem
 
 # -- Project information -----------------------------------------------------
 
