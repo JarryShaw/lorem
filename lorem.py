58430c6048257810a2049fcea32558ab20104f76
Jarry Shaw <jarryshaw@icloud.com>
2023-06-15 17:22:38 +0800

    bumped version to 1.3.0.post1

--- lorem.py
+++ lorem.py
@@ -87,7 +87,7 @@ __all__ = [
 ]
 
 # version string
-__version__ = '1.3.0'
+__version__ = '1.3.0.post1'
 
 #: The original lorem ipsum text pool.
 _TEXT = ('ad', 'adipiscing', 'aliqua', 'aliquip', 'amet', 'anim', 'aute', 'cillum', 'commodo',
